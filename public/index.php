80691e1a53836e3413d9b2c3a856a394fe71015e
Ars8 <pogosov-91@mail.ru>
2023-10-11 18:41:16 +0300

    composer

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
+require '../vendor/autoload.php';
+
+use Aura\SqlQuery\QueryFactory;
+
+$queryFactory = new QueryFactory('mysql');
+$select = $queryFactory->newSelect();
+
+var_dump($queryFactory);
+
 echo 123;
